c64a92efed3b2248b3ac9d2506738ea5fe8dc55b
Julien Nicot <61748457+juliennicotpixim@users.noreply.github.com>
2021-01-18 19:46:16 +0100

    Update user.js

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,3 +1,4 @@
+var bouncer = require ("express-bouncer")(500, 900000);     // importation du paquet express-bouncer pour luter contre attaques par force brute
 const express = require('express');     // importation du paquet express
 const router = express.Router();        // création du router
 
@@ -6,6 +7,6 @@ const userCtrl = require('../controllers/user')     // importation du controller
 const verifyPassword = require('../middleware/verify-password');        // importation du middleware
 
 router.post('/signup', verifyPassword, userCtrl.signup);
-router.post('/login', userCtrl.login);
+router.post('/login', bouncer.block ,userCtrl.login);
 
 module.exports = router;        // on export le router du fichier
